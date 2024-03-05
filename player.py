c08a2f3273c86046c2ba29f23ce5140af668dd60
melkerrr <162157794+melkerrr@users.noreply.github.com>
2024-03-05 21:33:20 +0100

    changes to the player class to allow name change

--- player.py
+++ player.py
@@ -2,7 +2,17 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
     def reset_score(self):
         self.score = 0
+
     def update_score(self, points):
         self.score += points
