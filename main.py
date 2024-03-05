9dfd807837944c0636b5c82a287bd099d7b039e9
melkerrr <162157794+melkerrr@users.noreply.github.com>
2024-03-05 18:42:57 +0100

    improved logic in main function

--- main.py
+++ main.py
@@ -29,17 +29,6 @@ def main():
 
         if choice == '1':
             game.play_game()
-            print('\nGame Over!')
-            print(f'\n{player1.name}: {player1.score}')
-            if player2:
-                print(f'{player2.name}: {player2.score}')
-            if player1.score > player2.score:
-                winner = player1.name
-            elif player2.score > player1.score:
-                winner = player2.name if player2 else 'Computer'
-            else:
-                winner = "It's a draw!"
-            print(f'Winner: {winner}')
             stats.update_stats(player1.name, player1.score)
             if player2:
                 stats.update_stats(player2.name, player2.score)
