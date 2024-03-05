c7ddc7cd3df983a1292c383bae2c0a92ab6488aa
melkerrr <162157794+melkerrr@users.noreply.github.com>
2024-03-05 15:45:17 +0100

    Add files via upload
    fixed bug in the update_stats method in the stats class

--- stats.py
+++ stats.py
@@ -6,8 +6,9 @@ class Stats:
         self.players[player.name] = {'score': player.score, 'games_played': 0}
 
     def update_stats(self, player_name, score):
-        self.players[player_name]['score'] += score
-        self.players[player_name]['games_played'] += 1
+        if player_name in self.players:
+            self.players[player_name]['score'] += score
+            self.players[player_name]['games_played'] += 1
 
     def get_high_scores(self):
         return sorted(self.players.items(), key=lambda x: x[1]['score'], reverse=True)
