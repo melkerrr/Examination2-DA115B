9c0608d23fc14c309792b94a0640c50065a763d6
melkerrr <162157794+melkerrr@users.noreply.github.com>
2024-03-06 08:40:02 +0100

    Large change: added cmd support

--- main.py
+++ main.py
@@ -4,87 +4,100 @@ from stats import Stats
 from cheat import Cheat
 from difficulty import Difficulty
 from statsmanager import StatsManager
+import cmd
 
-def main():
-    print('Welcome to Pig Dice Game!')
+class CmdInterface(cmd.Cmd):
+    prompt = '>> '
+    intro = "Welcome to Pig Dice Game! Type 'help' for available commands."
 
-    stats_manager = StatsManager()
-    player1_name = input('Enter name for player 1: ')
-    player1 = Player(player1_name)
-    
-    while True:
-        print('\n1. Play Game')
-        print('2. View High Scores')
-        print('3. Rules')
-        print('4. Cheat')
-        print('5. Change player names')
-        print('6. Quit')
+    def __init__(self):
+        super().__init__()
+        self.stats_manager = StatsManager()
+        self.player1 = None
+        self.player2 = None
+        self.game = None
 
-        choice = input('Enter your choice: ')
+    def do_play(self, arg):
+        '""Start a new game""'
+        if not self.player1:
+            player1_name = input('Enter name for player 1: ')
+            self.player1 = Player(player1_name)
+        self.game_mode()
+        
+    def game_mode(self):
+        print('\nChoose game mode')
+        print('1. Play against computer')
+        print('2. Play as 2 players')
+        mode_choice = input('Enter your choice: ')
 
-        if choice == '1':
-            # choose the game mode
-            print('\nChoose game mode:')
-            print('1. Play against computer')
-            print('2. Play as 2 players')
-            mode_choice = input('Enter your choice: ')
-
-            if mode_choice == '1':
-                player2 = Player('Computer')
-                difficulty_level = input('Choose difficulty level (easy/hard): ').lower()
-                while difficulty_level not in ['easy', 'hard']:
-                    print("Difficulty level invalid. Please choose 'easy' or 'hard'.")
-                    difficulty_level = input('Choose difficulty level (easy/hard): ').lower()
-                player2.difficulty = Difficulty(difficulty_level)
-            elif mode_choice == '2':
-                player2_name = input('Enter name for player 2: ')
-                player2 = Player(player2_name)
-            else:
-                print('Choice invalid! Choose again.')
-                continue
-            
-            game = Game(player1, player2)
-            while not game.current_game_over:
-                game.play_game()
-                stats_manager.update_stats(player1.name, player1.score)
-                stats_manager.update_stats(player2.name, player2.score)
-                player1.reset_score()
-                player2.reset_score()
-                if not game.current_game_over:
-                    restart = input('Do you want to restart the game? (yes/no): ').lower()
-                    if restart != 'yes':
-                        break
-        elif choice == '2':
-            print('\nHigh Scores:')
-            high_scores = stats_manager.get_high_scores()
-            for i, (name, data) in enumerate(high_scores):
-                print(f"\n{i+1}. {name}: Score - {data['score']}, Games Played - {data['games_played']}")
-        elif choice == '3':
-            print('\nRules:')
-            print('The game of Pig is a simple two-player dice game.')
-            print('Each turn, a player rolls a six-sided dice.')
-            print('If they roll a 1, their turn ends and they score no points for that turn.')
-            print('If they roll any other number, it is added to their turn total and they can choose to roll again or hold.')
-            print('The first player to reach 100 points win.')
-        elif choice == '4':
-            cheat_player = input('Enter name of player to activate cheat for: ')
-            if cheat_player.lower() == player1.name.lower():
-                cheat_player_name = Cheat.activate_cheat(player1)
-                print(f'\nCheat activated for {cheat_player_name}.')
-            elif player2 and cheat_player.lower() == player2.name.lower():
-                cheat_player_name = Cheat.activate_cheat(player2)
-                print(f'\nCheat activated for {cheat_player_name}.')
-            else:
-                print('\nPlayer not found!')
-        elif choice == '5':
-            new_name = input('Enter new name for player 1: ')
-            player1.name = new_name
-            print(f'Player 1 changed name to: {player1.name}')
-        elif choice == '6':
-            print('See you soon!')
-            break
+        if mode_choice == '1':
+            self.player2 = Player('Computer')
+            difficulty_level = input('Choose difficulty level (easy/hard): ').lower()
+            while difficulty_level not in ['easy', 'hard']:
+                print("Difficulty invalid. Please choose 'easy' or 'hard'.")
+                difficulty_level = input('Choose difficulty level (easy/hard): ')
+            self.player2.difficulty = Difficulty(difficulty_level)
+        elif mode_choice == '2':
+            player2_name = input('Enter name for player 2: ')
+            self.player2 = Player(player2_name)
         else:
-            print('Not a valid choice! Please choose again.')
+            print('Choice invalid! Choose again.')
+            self.game_mode()
+
+        self.game = Game(self.player1, self.player2)
+        while not self.game.current_game_over:
+            self.game.play_game()
+            self.stats_manager.update_stats(self.player1.name, self.player1.score)
+            self.stats_manager.update_stats(self.player2.name, self.player2.score)
+            self.player1.reset_score()
+            self.player2.reset_score()
+            if not self.game.current_game_over:
+                restart = input('Do you want to restart the game? (yes/no): ').lower()
+                if restart != 'yes':
+                    break
+
+    def do_scores(self, arg):
+        '""View high scores""'
+        print('\nHigh Scores:')
+        high_scores = self.stats_manager.get_high_scores()
+        for i, (name, data) in enumerate(high_scores):
+            print(f"\n{i+1}. {name}: Score - {data['score']}, Games Played - {data['games_played']}")
+
+    def do_rules(self, arg):
+        '""View game rules""'
+        print('\nRules:')
+        print('The game of Pig is a simple 2-player dice game.')
+        print('Each turn, a player rolls a six-sided dice.')
+        print('If they roll a 1, their turn ends and they score no points for that turn.')
+        print('If they roll any other numbeer, it is added to their turn total and they can choose to roll again or hold.')
+        print('The first player to reach 100 points win.')
+
+    def do_cheat(self, arg):
+        '""Activate cheat for a player""'
+        cheat_player = input('Enter name of player to activate cheat for: ')
+        if cheat_player.lower() == self.player1.name.lower():
+            cheat_player_name = Cheat.activate_cheat(self.player1)
+            print(f'\nCheat activated for {cheat_player_name}.')
+        elif self.player2 and cheat_player.lower() == self.player2.name.lower():
+            cheat_player_name = Cheat.activate_cheat(self.player2)
+            print(f'\nCheat activated for {cheat_player_name}.')
+        else:
+            print('\nPlayer not found!')
+
+    def do_rename(self, arg):
+        """Change player 1's name"""
+        new_name = input('Enter new name for player 1: ')
+        self.player1.name = new_name
+        print(f'Player 1 changed name to: {self.player1.name}')
+
+    def do_quit(self, arg):
+        """Quit the game"""
+        print('See you soon!')
+        return True
+    
+def main():
+    cmd_interface = CmdInterface()
+    cmd_interface.cmdloop()
 
 if __name__ == '__main__':
     main()
