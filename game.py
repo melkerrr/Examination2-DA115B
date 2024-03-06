6bb8a93faa33fcd76489a624920fe8347896fe67
melkerrr <162157794+melkerrr@users.noreply.github.com>
2024-03-06 19:29:08 +0100

    pylint

--- game.py
+++ game.py
@@ -1,11 +1,25 @@
-import random
+"""
+Module containing the Game class representing the game logic.
+"""
 
+import random
 from difficulty import Difficulty
 from statsmanager import StatsManager
 
-
 class Game:
+    """
+    Class representing the game logic.
+    """
+
     def __init__(self, player1, player2=None, difficulty="easy"):
+        """
+        Initialize the Game object.
+
+        Args:
+            player1 (Player): The first player.
+            player2 (Player, optional): The second player. Defaults to None.
+            difficulty (str, optional): The difficulty level. Defaults to "easy".
+        """
         self.player1 = player1
         self.player2 = player2
         self.stats_manager = StatsManager()
@@ -14,24 +28,39 @@ class Game:
         self.current_player = player1
 
     def switch_player(self):
+        """
+        Switch the current player.
+        """
         if self.current_player == self.player1:
             self.current_player = self.player2 if self.player2 else self.player1
         else:
             self.current_player = self.player1
 
     def roll_dice(self):
+        """
+        Simulate rolling a six-sided dice.
+
+        Returns:
+            int: The result of the dice roll.
+        """
         return random.randint(1, 6)
 
     def display_score(self):
+        """
+        Display the current player's score.
+        """
         if self.current_player == self.player1:
             print(f"\n{self.player1.name}'s current score: {self.current_player.score}")
         elif self.current_player == self.player2:
             if self.player2:
                 print(f"\n{self.player2.name}'s current score: {self.player2.score}")
             else:
-                print("\nComputer's current score is not idsplayed during its turn.")
+                print("\nComputer's current score is not displayed during its turn.")
 
     def play_turn(self):
+        """
+        Play a turn of the game.
+        """
         points = 0
         while True:
             self.display_score()
@@ -73,6 +102,9 @@ class Game:
                             break
 
     def play_game(self):
+        """
+        Play the game until one of the players wins.
+        """
         while self.player1.score < 100 and (
             self.player2 is None or self.player2.score < 100
         ):
@@ -91,4 +123,7 @@ class Game:
         print(f"Winner: {winner}")
 
     def quit_game(self):
+        """
+        Quit the game.
+        """
         self.current_game_over = True
